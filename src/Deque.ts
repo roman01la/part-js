9edac3749bf26d51616920531058e2a141d2aac1
Roman Liutikov <roman01la@romanliutikov.com>
2018-01-17 15:02:37 +0200

    implement Deque

---
+++ src/Deque.ts
@@ -0,0 +1,20 @@
+class Deque<T> {
+  private coll: T[] = []
+  push(value: T): void {
+    this.coll.push(value)
+  }
+  peek(): T | null {
+    return this.coll.length > 0 ? this.coll[this.coll.length - 1] : null
+  }
+  pop(): T {
+    const ret = this.coll.pop()
+    if (ret !== undefined) {
+      return ret
+    } else {
+      throw new Error("No such element")
+    }
+  }
+  isEmpty(): boolean {
+    return this.coll.length === 0
+  }
+}
